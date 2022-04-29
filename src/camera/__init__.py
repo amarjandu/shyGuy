602bab944fde0efa9942f46f03b7db5bc82d58f7
amar jandu <amarjandu@specterbyte.com>
2022-04-29 00:35:32 -0700

    Lock camera resolution for performance

--- src/camera/__init__.py
+++ src/camera/__init__.py
@@ -18,11 +18,15 @@ class Camera:
     """
     camera_number: int
     video_capture: typing.Optional[cv2.VideoCapture] = None
+    width: int = 800
+    height: int = 600
 
     def __post_init__(self):
         self.video_capture = cv2.VideoCapture(self.camera_number)
         if not self.read():
             raise ConnectionError('Unable to connect to camera: ', self.camera_number)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
 
     # TODO: Might want to encapsulate this within a context manager so
     #       VideoCapture.release() does not have to be called.
